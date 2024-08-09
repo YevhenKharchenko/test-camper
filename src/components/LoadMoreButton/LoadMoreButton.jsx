e0be6694d1b88b0bb543ddb557172fa9f0add7dd
YevhenKharchenko <evgenvalerych@gmail.com>
2024-08-09 23:49:55 +0300

    fix pagination

--- src/components/LoadMoreButton/LoadMoreButton.jsx
+++ src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -1,24 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import toast from 'react-hot-toast';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectPage } from '../../redux/selectors.js';
 import { fetchMoreCampers } from '../../redux/campers/operations.js';
 import s from './LoadMoreButton.module.css';
-import { resetPage } from '../../redux/campers/slice.js';
 
 const LoadMoreButton = () => {
   const dispatch = useDispatch();
   const page = useSelector(selectPage);
 
+  const [hasMounted, setHasMounted] = useState(false);
+
   const lastPage = page === 'lastPage';
-  console.log(lastPage);
 
   useEffect(() => {
-    if (lastPage) {
+    if (hasMounted && lastPage) {
       toast.error('There are no more campers to load.');
     }
-  }, [lastPage]);
+  }, [lastPage, hasMounted]);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
 
   const handleMoreBtnClick = () => {
     dispatch(fetchMoreCampers(page));
