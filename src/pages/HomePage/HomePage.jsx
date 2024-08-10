da4eba0a25ee6b92a184ed8c95fbc21bb706eff2
YevhenKharchenko <evgenvalerych@gmail.com>
2024-08-10 10:26:42 +0300

    add home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
 import DocumentTitle from '../../components/DocumentTitle.jsx';
+import { default as video } from '../../assets/video/slovenia.mov';
+import s from './HomePage.module.css';
 
 const HomePage = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play;
+    }
+  }, []);
+
   return (
     <div>
       <DocumentTitle>Rent Camper</DocumentTitle>
+      <div className={s.container}>
+        <video ref={videoRef} className={s.video} autoPlay muted loop playsInline>
+          <source type="video/mp4" src={video} />
+        </video>
+        <h1 className={s.title}>Your Journey, Your Way</h1>
+        <p className={s.text}>
+          Welcome to Rent Camper, where every road leads to adventure. We offer a range of camper
+          rentals to suit every traveler, from solo adventurers to families. Customize your journey,
+          explore at your own pace, and discover the beauty of the world around you. Your adventure
+          begins here!
+        </p>
+      </div>
     </div>
   );
 };
