b2367ca424e2f64695969903366c602003f07740
YevhenKharchenko <evgenvalerych@gmail.com>
2024-08-11 20:40:38 +0300

    fix header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Toaster } from 'react-hot-toast';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 
+import Navigation from './components/Navigation/Navigation.jsx';
 import SharedLayout from './components/SharedLayout/SharedLayout.jsx';
-import HourglassLoader from './shared/components/HourglassLoader/HourglassLoader.jsx';
 
-const Navigation = lazy(() => import('./components/Navigation/Navigation.jsx'));
 const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
 const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage.jsx'));
 const FavoritePage = lazy(() => import('./pages/FavoritePage/FavoritePage.jsx'));
@@ -17,9 +16,7 @@ function App() {
 
   return (
     <>
-      <Suspense fallback={<HourglassLoader />}>
-        <Navigation isHomePage={isHomePage} />
-      </Suspense>
+      <Navigation isHomePage={isHomePage} />
       <SharedLayout>
         <Toaster position="top-right" />
         <Routes>
